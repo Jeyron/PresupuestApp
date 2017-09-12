a5cf7c71d1c21eb52cc026599dfd8fa6eab9a8f5
Ronald Alfonso Rodriguez Centeno <rcrodriguez@estudiantec.cr>
2017-09-12 05:01:59 -0600

    Nuevo e importante (creación base de datos)

---
+++ app/src/main/java/com/example/jeiro/presupuestapp/base_datos.java
@@ -0,0 +1,93 @@
+package com.example.jeiro.presupuestapp;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+/**
+ * Created by rcrodriguez on 12/9/2017.
+ */
+
+public class base_datos extends SQLiteOpenHelper
+{
+    private static final String TEXT_TYPE = " TEXT";
+    private static final String NUMBER_TYPE = " INTEGER";
+    private static final String DATE_TYPE = " TEXT";
+
+    private static final String COMMA_SEP = ",";
+
+    private static final String SQL_CREATE_CATEGORIA =
+            "CREATE TABLE " + 
+                    tablas_base_datos.tablaCategoria.TABLE_NAME + " (" +
+                    tablas_base_datos.tablaCategoria._ID + " INTEGER PRIMARY KEY AUTOINCREMENT," +
+                    tablas_base_datos.tablaCategoria.COLUMN_NAME_TIPO + TEXT_TYPE + COMMA_SEP +
+                    tablas_base_datos.tablaCategoria.COLUMN_NAME_DESCRIPCION + TEXT_TYPE + COMMA_SEP +
+                    tablas_base_datos.tablaCategoria.COLUMN_NAME_EGRESO + TEXT_TYPE + COMMA_SEP +
+                    tablas_base_datos.tablaCategoria.COLUMN_NAME_MONTO + NUMBER_TYPE + COMMA_SEP +
+                    tablas_base_datos.tablaCategoria.COLUMN_NAME_MES + NUMBER_TYPE + " )";
+
+    private static final String SQL_CREATE_MES =
+            "CREATE TABLE " +
+                    tablas_base_datos.tablaMes.TABLE_NAME + " (" +
+                    tablas_base_datos.tablaMes._ID + " INTEGER PRIMARY KEY AUTOINCREMENT," +
+                    tablas_base_datos.tablaMes.COLUMN_NAME_NOMBRE + TEXT_TYPE + COMMA_SEP +
+                    tablas_base_datos.tablaMes.COLUMN_NAME_INGRESO + TEXT_TYPE + COMMA_SEP +
+                    tablas_base_datos.tablaMes.COLUMN_NAME_ESTADO + TEXT_TYPE + " )";
+
+    private static final String SQL_CREATE_EGRESO =
+            "CREATE TABLE " +
+                    tablas_base_datos.tablaEgreso.TABLE_NAME + " (" +
+                    tablas_base_datos.tablaEgreso._ID + " INTEGER PRIMARY KEY AUTOINCREMENT," +
+                    tablas_base_datos.tablaEgreso.COLUMN_NAME_IDENTIFICADOR_CATEGORIA + NUMBER_TYPE + COMMA_SEP +
+                    tablas_base_datos.tablaEgreso.COLUMN_NAME_FECHA + DATE_TYPE + COMMA_SEP +
+                    tablas_base_datos.tablaEgreso.COLUMN_NAME_HORA + DATE_TYPE + COMMA_SEP +
+                    tablas_base_datos.tablaEgreso.COLUMN_NAME_LOCAL + TEXT_TYPE + COMMA_SEP +
+                    tablas_base_datos.tablaEgreso.COLUMN_NAME_METODO_PAGO + TEXT_TYPE + COMMA_SEP +
+                    tablas_base_datos.tablaEgreso.COLUMN_NAME_MONTO + NUMBER_TYPE + COMMA_SEP +
+                    tablas_base_datos.tablaEgreso.COLUMN_NAME_DESCRIPCION + TEXT_TYPE + COMMA_SEP +
+                    tablas_base_datos.tablaEgreso.COLUMN_NAME_FACTURA + NUMBER_TYPE + COMMA_SEP +
+                    tablas_base_datos.tablaEgreso.COLUMN_NAME_TARJETA + NUMBER_TYPE + " )";
+
+    private static final String SQL_CREATE_TARJETA =
+            "CREATE TABLE " +
+                    tablas_base_datos.tablaTarjeta.TABLE_NAME + " (" +
+                    tablas_base_datos.tablaTarjeta.COLUMN_NAME_MARCA + TEXT_TYPE + COMMA_SEP +
+                    tablas_base_datos.tablaTarjeta.COLUMN_NAME_ENTIDAD + TEXT_TYPE + COMMA_SEP +
+                    tablas_base_datos.tablaTarjeta.COLUMN_NAME_TIPO + TEXT_TYPE + COMMA_SEP +
+                    tablas_base_datos.tablaTarjeta.COLUMN_NAME_NUMERO + NUMBER_TYPE + " PRIMARY KEY )";
+
+    private static final String SQL_DELETE_CATEGORIA =
+            "DROP TABLE IF EXISTS " + tablas_base_datos.tablaCategoria.TABLE_NAME;
+
+    private static final String SQL_DELETE_MES =
+            "DROP TABLE IF EXISTS " + tablas_base_datos.tablaMes.TABLE_NAME;
+
+    private static final String SQL_DELETE_EGRESO =
+            "DROP TABLE IF EXISTS " + tablas_base_datos.tablaEgreso.TABLE_NAME;
+
+    private static final String SQL_DELETE_TARJETA =
+            "DROP TABLE IF EXISTS " + tablas_base_datos.tablaTarjeta.TABLE_NAME;
+
+    public static final int DATABASE_VERSION = 1;
+    public static final String DATABASE_NAME = "Organizate.db";
+    public base_datos(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL(SQL_CREATE_CATEGORIA);
+        db.execSQL(SQL_CREATE_EGRESO);
+        db.execSQL(SQL_CREATE_MES);
+        db.execSQL(SQL_CREATE_TARJETA);
+    }
+
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL(SQL_DELETE_CATEGORIA);
+        db.execSQL(SQL_DELETE_MES);
+        db.execSQL(SQL_DELETE_EGRESO);
+        db.execSQL(SQL_DELETE_TARJETA);
+        onCreate(db);
+    }
+    public void onDowngrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        onUpgrade(db, oldVersion, newVersion);
+    }
+}
---
+++ app/src/main/java/com/example/jeiro/presupuestapp/tablas_base_datos.java
@@ -0,0 +1,48 @@
+package com.example.jeiro.presupuestapp;
+
+import android.provider.BaseColumns;
+
+/**
+ * Created by rcrodriguez on 12/9/2017.
+ */
+
+public final class tablas_base_datos {
+    private tablas_base_datos() {}
+
+    public static class tablaCategoria implements BaseColumns {
+        public static final String TABLE_NAME                = "Categoria";
+        public static final String COLUMN_NAME_TIPO          = "Tipo";
+        public static final String COLUMN_NAME_DESCRIPCION   = "Descripcion";
+        public static final String COLUMN_NAME_EGRESO       = "Egreso";
+        public static final String COLUMN_NAME_MONTO         = "Monto";
+        public static final String COLUMN_NAME_MES           = "Mes";
+    }
+
+    public static class tablaMes implements BaseColumns {
+        public static final String TABLE_NAME                = "Mes";
+        public static final String COLUMN_NAME_NOMBRE        = "Nombre";
+        public static final String COLUMN_NAME_ESTADO        = "Estado";
+        public static final String COLUMN_NAME_INGRESO       = "Ingreso";
+    }
+
+    public static class tablaTarjeta implements BaseColumns {
+        public static final String TABLE_NAME                = "Tarjeta";
+        public static final String COLUMN_NAME_ENTIDAD       = "Entidad";
+        public static final String COLUMN_NAME_TIPO          = "Tipo";
+        public static final String COLUMN_NAME_NUMERO        = "Numero";
+        public static final String COLUMN_NAME_MARCA         = "Marca";
+    }
+
+    public static class tablaEgreso implements BaseColumns {
+        public static final String TABLE_NAME                           = "Egreso";
+        public static final String COLUMN_NAME_IDENTIFICADOR_CATEGORIA  = "IdentificadorCategoria";
+        public static final String COLUMN_NAME_FECHA                    = "Fecha";
+        public static final String COLUMN_NAME_HORA                     = "Hora";
+        public static final String COLUMN_NAME_LOCAL                    = "Local";
+        public static final String COLUMN_NAME_METODO_PAGO              = "MetodoPago";
+        public static final String COLUMN_NAME_MONTO                    = "Monto";
+        public static final String COLUMN_NAME_DESCRIPCION              = "Descripcion";
+        public static final String COLUMN_NAME_FACTURA                  = "Factura";
+        public static final String COLUMN_NAME_TARJETA                  = "Tarjeta";
+    }
+}
