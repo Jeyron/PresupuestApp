003f05bd65dc7e4927b88deaa4dbe9e96c2b61fc
Jeyron <31875131+Jeyron@users.noreply.github.com>
2017-09-13 00:47:05 -0600

    color y reportes

---
+++ app/src/main/java/com/example/jeiro/presupuestapp/Reporte_balance_categoria_egreso.java
@@ -0,0 +1,13 @@
+package com.example.jeiro.presupuestapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Reporte_balance_categoria_egreso extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_reporte_balance_categoria_egreso);
+    }
+}
---
+++ app/src/main/java/com/example/jeiro/presupuestapp/Reporte_resumen_gastos_tarjeta.java
@@ -0,0 +1,13 @@
+package com.example.jeiro.presupuestapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Reporte_resumen_gastos_tarjeta extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_reporte_resumen_gastos_tarjeta);
+    }
+}
