6965bfe53df837b1800805696ec301a4faec68af
Ronald Alfonso Rodriguez Centeno <rcrodriguez@estudiantec.cr>
2017-09-14 10:11:43 -0600

    mmmm

--- app/src/main/java/com/example/jeiro/presupuestapp/Registro_egreso_mensual.java
+++ app/src/main/java/com/example/jeiro/presupuestapp/Registro_egreso_mensual.java
@@ -3,10 +3,12 @@ package com.example.jeiro.presupuestapp;
 import android.content.Context;
 import android.net.Uri;
 import android.os.Bundle;
+import android.support.v4.app.DialogFragment;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
 
 
 /**
@@ -104,5 +106,11 @@ public class Registro_egreso_mensual extends Fragment {
     public interface OnFragmentInteractionListener {
         // TODO: Update argument type and name
         void onFragmentInteraction(Uri uri);
+    }
+
+    public void showDatePickerDialog(View v)
+    {
+        DialogFragment newFragmentDate = new Date_Picker();
+        newFragmentDate.show(getFragmentManager(), "Date Picker");
     }
 }
